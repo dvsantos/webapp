cd1413f53326b250131618135e7b09dbbdf8f786
Diogo Vernier <vernisan@gmail.com>
2013-09-03 19:28:58 -0300

    teste

--- src/main/java/com/sandbox/controller/TestController.java
+++ src/main/java/com/sandbox/controller/TestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.sandbox.test.CutBar;
 import com.sandbox.test.CutItem;
@@ -22,16 +23,56 @@ public class TestController {
 		Map<CutItem, Integer> demand = new HashMap<>();
 		
 		for(CutBar bar : bars) {
-			patterns.add(getBarPatterns(bar, demand));
+			//patterns.add(getBarPatterns(bar, demand));
+			
+			
+			
 		}
 		
 	}
 
-	private static CutPattern getBarPatterns(CutBar bar, Map<CutItem, Integer> demand) {
+	
+	
+	
+	
+	//problemas de math com Double
+	private static List<CutPattern> getBarPatterns(CutBar bar, Map<CutItem, Integer> demand) {
+
+		List<CutPattern> patterns = new ArrayList<>();
 		
+		Double barSize = bar.getSize();
 		
-		
-		
+		for (Entry<CutItem, Integer> entry : demand.entrySet()) {
+
+			CutItem item = entry.getKey();
+			
+			Integer required = entry.getValue();
+			
+			for(int s = required; s >= 0; s--) {
+				
+				Double occupiedSize = s * item.getSize();
+				
+				if(occupiedSize == barSize) {
+					// perfect fit
+					
+				} else if((occupiedSize > barSize)) {
+					// discard and continue
+					
+				} else {
+					// call recursion
+					CutBar remainingBar = new CutBar();
+					remainingBar.setSize(barSize - occupiedSize);
+					
+					Map<CutItem, Integer> remainingDemand = new HashMap<>(demand);
+					remainingDemand.remove(item);
+					
+					patterns.addAll(getBarPatterns(remainingBar, remainingDemand));
+				}
+				
+			}
+			
+		}
+
 		return null;
 	}
 	
